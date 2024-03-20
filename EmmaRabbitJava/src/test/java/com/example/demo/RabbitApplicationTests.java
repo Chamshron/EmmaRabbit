4af1d7ea63ce467409866d08b0da82d5e52b3756
Chamsron <emmacameron219@gmail.com>
2024-03-20 13:09:34 +0000

    created repository

--- EmmaRabbitJava/src/test/java/com/example/demo/RabbitApplicationTests.java
+++ EmmaRabbitJava/src/test/java/com/example/demo/RabbitApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import com.example.demo.models.Rabbit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RabbitApplicationTests {
+public class RabbitApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+	}
+
+	@Test
+	public void cannotPurchaseRabbitIfQuantityIsZero(){
+		Rabbit rabbit = new Rabbit("Emma", "Dance Party", 100, 1);
+		rabbit.purchaseRabbit();
+		rabbit.purchaseRabbit();
 	}
 
 }
