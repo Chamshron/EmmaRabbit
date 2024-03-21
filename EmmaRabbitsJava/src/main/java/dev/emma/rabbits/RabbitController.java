0642acffeccb8c47780082c744f966a2dbed9e95
Chamsron <emmacameron219@gmail.com>
2024-03-21 13:07:13 +0000

    seeded data appearing in localhost browser

--- EmmaRabbitsJava/src/main/java/dev/emma/rabbits/RabbitController.java
+++ EmmaRabbitsJava/src/main/java/dev/emma/rabbits/RabbitController.java
@@ -1,5 +1,6 @@
 package dev.emma.rabbits;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,11 +8,15 @@ import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/rabbits")
 public class RabbitController {
+    @Autowired
+    private RabbitService rabbitService;
     @GetMapping
-    public ResponseEntity<String> getAllRabbits(){
-        return new ResponseEntity<String>("all the rabbits", HttpStatus.OK);
+    public ResponseEntity<List<Rabbit>> getAllRabbits(){
+        return new ResponseEntity<List<Rabbit>>(rabbitService.getAllRabbits(), HttpStatus.OK);
     }
 }
--- EmmaRabbitsJava/src/main/java/dev/emma/rabbits/RabbitService.java
+++ EmmaRabbitsJava/src/main/java/dev/emma/rabbits/RabbitService.java
@@ -1,11 +1,13 @@
 package dev.emma.rabbits;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class RabbitService {
+    @Autowired
     private RabbitRepository rabbitRepository;
     public List<Rabbit> getAllRabbits(){
         return rabbitRepository.findAll();
