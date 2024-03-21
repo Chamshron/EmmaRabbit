e5796f8004788c50aa421cf3616afb4297c2b72b
Chamsron <emmacameron219@gmail.com>
2024-03-21 11:24:48 +0000

    setting up to use Lombek

--- EmmaRabbitsJava/src/main/java/dev/emma/rabbits/Rabbit.java
+++ EmmaRabbitsJava/src/main/java/dev/emma/rabbits/Rabbit.java
@@ -1,51 +1,20 @@
 package dev.emma.rabbits;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="rabbits")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Rabbit {
     private ObjectId id;
     private String series;
     private String name;
     private String odds;
 
-    public Rabbit(ObjectId id, String series, String name, String odds) {
-        this.id = id;
-        this.series = series;
-        this.name = name;
-        this.odds = odds;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getSeries() {
-        return series;
-    }
-
-    public void setSeries(String series) {
-        this.series = series;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getOdds() {
-        return odds;
-    }
-
-    public void setOdds(String odds) {
-        this.odds = odds;
-    }
+    
 }
