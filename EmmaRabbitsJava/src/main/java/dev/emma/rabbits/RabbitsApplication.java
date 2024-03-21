0333af8a10679131cbefe27a07430ca24289c8e4
Chamsron <emmacameron219@gmail.com>
2024-03-21 11:05:56 +0000

    connecting to mongodb db

--- EmmaRabbitsJava/src/main/java/dev/emma/rabbits/RabbitsApplication.java
+++ EmmaRabbitsJava/src/main/java/dev/emma/rabbits/RabbitsApplication.java
@@ -2,12 +2,16 @@ package dev.emma.rabbits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RabbitsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitsApplication.class, args);
 	}
+
 
 }
